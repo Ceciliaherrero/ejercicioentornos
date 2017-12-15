ca0bfc0941fa3e1e16c621e50c3e1acafa8a2543
Equipo <Equipo@Equipo-PC>
2017-12-15 22:42:00 +0100

    Clase simple creada.

---
+++ Actividad3CeciliaHerrero/src/actividad3ceciliaherrero/clasesimple.java
@@ -0,0 +1,17 @@
+
+package actividad3ceciliaherrero;
+
+/**
+ *
+ * @author Equipo
+ */
+public class clasesimple {
+    
+    private String nombre;
+    private String apellido;
+    
+    public clasesimple (){
+        this.nombre="Cecilia";
+        this.apellido="Herrero";
+    }
+}
