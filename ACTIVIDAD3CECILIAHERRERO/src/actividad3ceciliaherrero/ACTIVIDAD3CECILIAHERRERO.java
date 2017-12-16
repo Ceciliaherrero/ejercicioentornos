6315ace640f05d8be6f129fa80bdeb6dca668293
Equipo <Equipo@Equipo-PC>
2017-12-16 10:33:50 +0100

    He creado una clase simple

---
+++ ACTIVIDAD3CECILIAHERRERO/src/actividad3ceciliaherrero/ACTIVIDAD3CECILIAHERRERO.java
@@ -0,0 +1,23 @@
+
+package actividad3ceciliaherrero;
+
+/**
+ *
+ * @author Equipo
+ */
+public class ACTIVIDAD3CECILIAHERRERO {
+
+    private String nombre;
+    private String clase;
+    
+    public ACTIVIDAD3CECILIAHERRERO (){
+        this.nombre ="Cecilia";
+        this.clase="daw1";
+}
+    
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
